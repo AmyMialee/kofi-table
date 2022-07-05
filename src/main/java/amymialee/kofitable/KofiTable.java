be793520f4736d8bf0d2bfaf4f0fe905ade452f9
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2022-07-05 17:09:21 +0100

    mod

---
+++ src/main/java/amymialee/kofitable/KofiTable.java
@@ -0,0 +1,10 @@
+package amymialee.kofitable;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KofiTable implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/amymialee/kofitable/KofiTableClient.java
@@ -0,0 +1,13 @@
+package amymialee.kofitable;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class KofiTableClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
